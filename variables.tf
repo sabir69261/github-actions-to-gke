6e702444dc923283585853f9718dce7e48ec3939
Mohd Sabir <sabir69261@gmail.com>
2023-03-25 10:53:17 +0530

    update_variables_file

--- variables.tf
+++ variables.tf
@@ -158,22 +158,6 @@ variable "healthcheck_ranges" {
   type        = set(string)  
 }
 
-# Service account 
-
-variable "common_account_id" {
-  type    = string
-  description = "The account id that is used to generate the service account email address and a stable unique id."
-}
-
-variable "common_display_name" {
-  type    = string
-  description = "The display name for the service account"
-}
-
-variable "common_description" {
-  type    = string
-  description = "A text description of the service account"
-}
 
 # GKE
 variable "name" {
